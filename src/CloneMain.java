03ed73a7e31b8b6d52294de6ebf8e9f587fec76f
MsRioux <jrioux@mvusd.net>
2019-08-19 16:51:01 -0700

    Initial Commit

---
+++ src/CloneMain.java
@@ -0,0 +1,16 @@
+public class CloneMain
+{
+        public static void main(String args[])
+        {
+            //Instance Variables
+            int age = 44;
+            int height = 64 ;
+            String name = "Ms. Rioux", animal = "unicorn";
+
+            // Output
+            System.out.println("Hello, my name is " + name + " and I am " + age + " years old.");
+            System.out.println("I am " + height + " inches tall and I wish I was a " + animal);
+
+        }// end main method
+    }// end class CloneMain
+
